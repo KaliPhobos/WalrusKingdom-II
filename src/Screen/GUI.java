2a0cc94e9ef1a4cad536b3913575f2000431fc81
p2 <p2abc@icloud.com>
2020-08-01 04:09:27 +0200

    prepared smooth GUI resizing

--- src/Screen/GUI.java
+++ src/Screen/GUI.java
@@ -15,60 +15,126 @@ public class GUI {
 	}
 	
 	public void setPxlWidth(int pxlWidth) {
+		// Set the frame's width using pixels as a unit.
+		// Will force a GUI reset
+		pxlWidth = confirm(pxlWidth);
 		this.pxlWidth = pxlWidth;
 		this.tileWidth = pxlWidth/tilesize;
 		GUIreset();
 	}
+	
+
+	
 	public void setPxlWidth(int pxlWidth, boolean doRedraw) {
+		// Set the frame's width using pixels as a unit.
+		// May force a GUI reset depending on the given parameter (which is recommended)
+		pxlWidth = confirm(pxlWidth);
 		this.pxlWidth = pxlWidth;
 		this.tileWidth = pxlWidth/tilesize;
 		if (doRedraw) {
 			GUIreset();
 		}
 	}
+	
 	public void setPxlHeight(int pxlHeight) {
+		// Set the frame's height using pixels as a unit.
+		// Will force a GUI reset
+		pxlHeight = confirm(pxlHeight);
 		this.pxlHeight = pxlHeight;
 		this.tileHeight = pxlHeight/tilesize;
 		GUIreset();
 	}
+	
 	public void setPxlHeight(int pxlHeight, boolean doRedraw) {
+		// Set the frame's width using pixels as a unit.
+		// May force a GUI reset depending on the given parameter (which is recommended)
+		pxlHeight = confirm(pxlHeight);
 		this.pxlHeight = pxlHeight;
 		this.tileHeight = pxlHeight/tilesize;
 		if (doRedraw) {
 			GUIreset();
 		}
 	}
+	
 	public void setTileWidth(int tileWidth) {
+		// Set the frame's width using tiles as a unit (X * this.tilesize).
+		// Will force a GUI reset
 		this.tileWidth = tileWidth;
 		setPxlWidth(tileWidth*tilesize);
 	}
+	
+	public void setTileWidth(int tileWidth, boolean doRedraw) {
+		// Set the frame's width using tiles as a unit (X * this.tilesize).
+		// May force a GUI reset depending on the given parameter (which is recommended)
+		this.tileWidth = tileWidth;
+		setPxlWidth(tileWidth*tilesize, doRedraw);
+	}
+	
 	public void setTileHeight(int tileHeight) {
+		// Set the frame's height using tiles as a unit (X * this.tilesize).
+		// Will force a GUI reset
 		this.tileHeight = tileHeight;
 		setPxlHeight(tileHeight*tilesize);
 	}
+	
+	public void setTileHeight(int tileHeight, boolean doRedraw) {
+		// Set the frame's width using tiles as a unit (X * this.tilesize).
+		// May force a GUI reset depending on the given parameter (which is recommended)
+		this.tileHeight = tileHeight;
+		setPxlHeight(tileHeight*tilesize, doRedraw);
+	}
+	
 	public void setTilesize(int tilesize) {
+		// Set the frame's tile size, using pixels as a unit.
+		// Will force a GUI reset
 		this.tilesize = tilesize;
 		setPxlWidth(tileWidth*tilesize, false);
 		setPxlHeight(tileHeight*tilesize, true);	// force full redraw
 	}
+	
+	public void setTilesize(int tilesize, boolean doRedraw) {
+		// Set the frame's tile size, using pixels as a unit.
+		// May force a GUI reset depending on the given parameter
+		this.tilesize = tilesize;
+		setPxlWidth(tileWidth*tilesize, doRedraw);
+		setPxlHeight(tileHeight*tilesize, doRedraw);	// force full redraw
+	}
+	
+	public int getTileWidth() {
+		return this.tileWidth;
+	}
+	
+	public int getTileHeight() {
+		return this.tileHeight;
+	}
+	
+	public int confirm(int value) {
+		// Check if a given value can be divided by this.tilesize. If not, paste an error message.
+		// Faulty values will be corrected to the next smaller (fitting) value.
+		int temp = value%tilesize;
+		if (temp!=0) {
+			System.out.println("Input was adapted to fit parameters. ("+value+" -> "+(value-temp)+")");
+		}
+		return value - temp;
+	}
+	
 	public void GUIreset() {
 		// This method forces the JFrame element to be removed and then recreated
 		// Effectively this will do a hard GUI reset, fixing any temporary bug
 		if (this.frame!=null) {
 			// close old window so a new one can be opened
 		}
-		this.frame = createWindow();
+		createWindow();
 		// force full redraw
 	}
 	
-	
-	
-	private JFrame createWindow() {
+	private void createWindow() {
 		// Pretty obvious, it *creates window*
+		System.out.println("A new window is being created");
 		JFrame frame = new JFrame("WalrusKingdom "+this.pxlWidth+"x"+this.pxlHeight);
         frame.setIgnoreRepaint(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(this.pxlWidth, this.pxlHeight);		// set window size
-	    return frame;
+		// frame.setSize(this.pxlWidth, this.pxlHeight);		// set window size
+	    this.frame = frame;
 	}
 }
--- src/main/main.java
+++ src/main/main.java
@@ -9,14 +9,14 @@ import Screen.Screen;
 public class main {
 	public static void main(String[] args) {
 		GUI gui = new GUI();
-		gui.setTileWidth(31);			// game resolution
-		gui.setTileHeight(17);
-		gui.setTilesize(24);			// size of tiles used in the game (24px)
+		gui.setTileWidth(31, false);		// game resolution
+		gui.setTileHeight(17, false);
+		gui.setTilesize(24, false);				// size of tiles used in the game (24px)
 		
 		
 		
 		Map townMap = new Map(80, 40);
-		Screen previewScreen = new Screen (12, 8);
+		Screen previewScreen = new Screen(gui.getTileWidth(), gui.getTileHeight());
 		previewScreen.update(townMap);
 		
 		System.out.println(previewScreen.preview());
